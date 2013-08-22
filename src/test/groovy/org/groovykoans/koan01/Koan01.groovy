1bc949834ec82847e8d31f8beaf2f6dca95556ee
giacung <giao.the.cung@finn.no>
2013-08-22 17:54:33 +0200

    Koan01.

--- src/test/groovy/org/groovykoans/koan01/Koan01.groovy
+++ src/test/groovy/org/groovykoans/koan01/Koan01.groovy
@@ -42,7 +42,8 @@ class Koan01 extends GroovyTestCase {
 
         // Assign our variables the required values to continue...
         // ------------ START EDITING HERE ----------------------
-
+        assertion = true
+        hello = 'Hello'
 
         // ------------ STOP EDITING HERE  ----------------------
 
@@ -64,7 +65,7 @@ class Koan01 extends GroovyTestCase {
         // Create the target string with the ${} mechanism. Remember that ${} can contain method calls too!
         String result
         // ------------ START EDITING HERE ----------------------
-
+        result = "The size of the string '${greeting}' is ${greeting.size()}"
 
         // ------------ STOP EDITING HERE  ----------------------
 
@@ -78,9 +79,9 @@ class Koan01 extends GroovyTestCase {
 
         // Concatenate the two values of 'right' and 'left' into result to proceed using Groovy syntax
         def result
+
         // ------------ START EDITING HERE ----------------------
-
-
+         result = map.left + map.right
         // ------------ STOP EDITING HERE  ----------------------
 
         assert result.toCharArray().size() == 16
@@ -102,6 +103,7 @@ class Koan01 extends GroovyTestCase {
         // Hint: you can use either Java's add(int, String) or Groovy's plus() method.
         // ------------ START EDITING HERE ----------------------
 
+        groovyList = groovyList.plus(1,'Queen')
 
         // ------------ STOP EDITING HERE  ----------------------
 
@@ -143,6 +145,7 @@ class Koan01 extends GroovyTestCase {
         def message
         // ------------ START EDITING HERE ----------------------
 
+        message = "Hello ${userService?.loggedInUser?.firstName?:'Anonymous'}!"
 
         // ------------ STOP EDITING HERE  ----------------------
 
